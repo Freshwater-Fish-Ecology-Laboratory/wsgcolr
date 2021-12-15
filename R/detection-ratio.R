355b637fe923532ede6ba802a7bab252257ec508
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-12-14 18:54:54 -0800

    remove old detection path code

--- R/detection-ratio.R
+++ R/detection-ratio.R
@@ -1,33 +1,5 @@
-#' Calculate detection residence.
+#' Get complete detection data (i.e. filling in absent receiver_group/timestep)
 #'
-#' Determine detection presence/absence for each transmitter/timestep/receiver group combination
-#' `timestep` determines how to sequence date(times) between event start and event end - ideally this should match what was used in `detection_timestep()`.
-#'
-#' @inheritParams params
-#' @return A tibble.
-#'
-#' @export
-detection_residence <- function(detection_event, timestep = "week", receiver_group = "receiver_group"){
-  
-  # chk_detection_timestep(detection_timestep)
-  chk_timestep(timestep)
-  chk_string(receiver_group)
-  
-  timestep_range <- seq.Date(min(detection_timestep$timestep), max(detection_timestep$timestep), by = timestep)
-  detection_timestep$present <- 1
-  detection_timestep %>%
-    arrange(transmitter, timestep, receiver_group) %>%
-    tidyr::complete(timestep = timestep_range,
-                    receiver_group, transmitter,
-                    fill = list(present = 0)) %>%
-    select(transmitter, receiver_group, timestep, present)
-}
-
-
-#' Calculate detection residence.
-#'
-#' Determine detection presence/absence for each transmitter/timestep/receiver group combination
-#' `timestep` should match what was used in `detection_timestep`.
 #'
 #' @inheritParams params
 #' @return A modified tibble of the complete detection data.
